240af9a8ff91471674d5b20d98471a1951fcf39e
Thiago Rabelo <thiagorabelo1@gmail.com>
2022-05-03 15:49:35 -0300

    Script para automatizar a criação do cluster

--- create.sh
+++ create.sh
@@ -3,8 +3,12 @@
 PG_VERSION="${PG_VERSION:-14}"
 PG_SLOT="${PG_SLOT:-replica_1_slot}"
 POSTGRES_PASSWORD="${POSTGRES_PASSWORD:-123@mudar}"
+PG_REPLICATION_USER=repl
+PG_NETWORK=${PG_NETWORK:-192.168.55.0/24}
 
 PG_CONTAINER_DATA="/var/lib/postgresql/data"
+
+dc="$(docker compose --help 2>&1 > /dev/null && echo "docker compose" || echo "docker-compose")"
 
 function wait_pg_running() {
     local container=${1}
@@ -17,55 +21,56 @@ function wait_pg_running() {
 
 function wait_pg_compose_running() {
     local service=${1}
-    until [[ "$(docker-compose exec -u postgres ${service} psql -qAt -c "select 1;" 2>/dev/null)"  ==  1 ]]; do
+    until [[ "$(${dc} exec -u postgres ${service} psql -qAt -c "select 1;" 2>/dev/null)"  ==  1 ]]; do
         echo "Aguardando ${service}"
         sleep 1;
     done;
 }
 
 
-# docker container run --name pg_temp -d -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" "postgres:${PG_VERSION}"
+docker container run --name pg_temp -d \
+    -e POSTGRES_PASSWORD="${POSTGRES_PASSWORD}" \
+    "postgres:${PG_VERSION}"
 
-# wait_pg_running pg_temp
+wait_pg_running pg_temp
 
-# docker container cp pg_temp:"${PG_CONTAINER_DATA}/postgresql.conf" primary/
-# docker container cp pg_temp:"${PG_CONTAINER_DATA}/pg_hba.conf" primary/
+docker container cp pg_temp:"${PG_CONTAINER_DATA}/postgresql.conf" primary/
+docker container cp pg_temp:"${PG_CONTAINER_DATA}/pg_hba.conf" primary/
 
-# docker container rm -f pg_temp
+docker container rm -f pg_temp
 
-# chmod 644 primary/postgresql.conf
-# chmod 644 primary/pg_hba.conf
+chmod 644 primary/postgresql.conf
+chmod 644 primary/pg_hba.conf
 
-# echo -e "\nwal_level = replica\nmax_wal_senders = 10\nwal_keep_size = '1GB'\nwal_compression = on" >> primary/postgresql.conf
-# echo -e "\nhost    replication     repl            192.168.55.0/24         trust\n" >> primary/pg_hba.conf
+echo -e "\nwal_level = replica\nmax_wal_senders = 10\nwal_keep_size = '1GB'\nwal_compression = on" >> primary/postgresql.conf
+echo -e "\nhost    replication     ${PG_REPLICATION_USER}            ${PG_NETWORK}         trust\n" >> primary/pg_hba.conf
 
-# docker-compose up -d primary
+${dc} up -d primary
 
-# wait_pg_compose_running primary
+wait_pg_compose_running primary
 
-# docker-compose exec -u postgres primary createuser --replication repl
-# docker-compose exec -u postgres primary psql -c "select * from pg_create_physical_replication_slot('${PG_SLOT}');"
+${dc} exec -u postgres primary createuser --replication ${PG_REPLICATION_USER}
+${dc} exec -u postgres primary \
+    psql -c "select * from pg_create_physical_replication_slot('${PG_SLOT}');"
 
-
-set -x
 
 touch ./replica/postgresql.conf
 touch ./replica/pg_hba.conf
 
-docker-compose run --rm \
+${dc} run --rm \
     --entrypoint bash \
     replica \
     -c "chown postgres:postgres /var/lib/postgresql/data"
 
-docker-compose run --rm -u postgres --name repl_temp \
+${dc} run --rm -u postgres --name repl_temp \
     -v $(pwd)/replica/data:${PG_CONTAINER_DATA}/../pgdata:rw \
     --entrypoint bash \
     replica \
     -c "pg_basebackup --pgdata ${PG_CONTAINER_DATA}/../pgdata \
     --format=p --write-recovery-conf --checkpoint=fast --label=replica-1 \
-    --progress --host=primary --port=5432 --username=repl"
+    --progress --host=primary --port=5432 --username=${PG_REPLICATION_USER}"
 
-docker-compose run --rm -u postgres --name repl_temp \
+${dc} run --rm -u postgres --name repl_temp \
     -v $(pwd)/replica/data:${PG_CONTAINER_DATA}/../pgdata:rw \
     --entrypoint bash \
     -d \
@@ -83,5 +88,7 @@ mv replica/pg_hba.conf.1 replica/pg_hba.conf
 chmod 644 replica/postgresql.conf
 chmod 644 replica/pg_hba.conf
 
-echo -e "\nprimary_conninfo = 'user=repl port=5432 host=primary application_name=replica-1'" >> replica/postgresql.conf
-echo -e "\nprimary_slot_name = 'replica_1_slot'" >> replica/postgresql.conf
+echo -e "\nprimary_conninfo = 'user=${PG_REPLICATION_USER} port=5432 host=primary application_name=replica-1'" >> replica/postgresql.conf
+echo -e "\nprimary_slot_name = '${PG_SLOT}'" >> replica/postgresql.conf
+
+${dc} down
